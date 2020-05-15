54523496cfcf5ca84abc03b682166c0e4e2017f1
Singh, Jasbeer <jasbeer.singh@barco.com>
2020-05-15 23:29:58 +0530

    checkbox functionality

--- htmlCSSAndJs/HW4_react_form/FontChooser.js
+++ htmlCSSAndJs/HW4_react_form/FontChooser.js
@@ -2,26 +2,28 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state = {hidden: true};
+	this.state = {hidden: true, bold: this.props.bold == 'true'};
     }
 	
 	onTextClick(){
 		this.setState({hidden: !this.state.hidden,})
 	}
 
+	handleCheckbox(){
+		this.setState({bold: !this.state.bold})
+	}
+
     render() {
 		var textStyle = {
-			fontWeight: (this.props.bold == 'true')? 'bold': 'normal',
+			fontWeight: this.state.bold? 'bold': 'normal',
 			fontSize: parseInt(this.props.size), 
 		}
-		// debugger;
-		// var hidden = this.state.hidden?'true':'false';
-		// debugger;
+		var checkbox_status = this.state.bold;
 
 	return(
 			
 	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden={this.state.hidden}/>
+	       <input type="checkbox" id="boldCheckbox" hidden={this.state.hidden} checked={checkbox_status} onChange={this.handleCheckbox.bind(this)}/>
 	       <button id="decreaseButton" hidden={this.state.hidden}>-</button>
 	       <span id="fontSizeSpan" hidden={this.state.hidden}>{this.props.size}</span>
 	       <button id="increaseButton" hidden={this.state.hidden}>+</button>
